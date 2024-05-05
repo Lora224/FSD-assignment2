ab3a24e78c8caf6689ed83d03ab9ef990d23c867
Lora224 <35753187+Lora224@users.noreply.github.com>
2024-05-05 23:05:19 +1000

    database.py updated

--- database.py
+++ database.py
@@ -1,7 +1,30 @@
 #database class goes here
-#CRUD of student.data
-#checks if student.data exists
-#creates student.data if it does not exist
-#write object to student.data
-#read object from student.data
-#clear student.data
+
+#checks if student.data exists √
+#creates student.data if it does not exist √
+#write object to student.data √
+#read object from student.data √
+#clear student.data √
+
+#verify student credentials?
+#update password?
+
+#subject, student controllers etc intereact with student.data
+
+import os
+class Database:
+    def __init__(self):
+        self.path = 'student.data'
+        if not os.path.isfile(self.path):
+            with open(self.path, "x") as file:
+                pass
+
+    def write(self, data):
+        with open(self.path, "a") as file:
+            file.write(data)
+    def read(self):
+        with open(self.path, "r") as file:
+            return file.readlines()
+    def clear(self):
+        with open(self.path, "w") as file:
+            pass
